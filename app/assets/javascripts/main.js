acf634a303e2780cba47ef9235da714883fc80c8
vincelep <vincent.leparoux@gmail.com>
2019-02-27 19:05:57 +0100

    horrible

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,35 @@
-document.addEventListener("turbolinks:load", function() {
- alert("ok!")
-});
+// document.addEventListener("turbolinks:load", function() {
+//  alert("ok!")
+// });
 
-$(function(){
-  $(".card").hide(function(){
-    $(".container").click(function(){
-      $(".card").show()
-    });
+// $(function(){
+//   $(".card").hide(function(){
+//     $(".container").click(function(){
+//       $(".card").show()
+//     });
+//   });
+// });
+
+
+// $(function(){
+//   $(".rounded-circle").toggle(function(){
+//     $(".nav-link")
+//   });
+// });
+
+$(() => {
+  // tabs
+  let tab_content = $('.tab_content')
+
+  tab_content.slice(1, tab_content.length).hide()
+
+  $('.tab_link').on( "click", function() {
+    tab_content.hide()
+    $(this).next().slideDown('slow')
   });
-});
 
-
-$(function(){
-  $(".rounded-circle").toggle(function(){
-    $(".nav-link")
+  // dropdown
+  $('button.navbar-toggler').on("click", function() {
+    $('#navbarTogglerDemo03').hasClass('collapse') ? $('#navbarTogglerDemo03').removeClass("collapse") : $('#navbarTogglerDemo03').addClass('collapse');
   });
 });
